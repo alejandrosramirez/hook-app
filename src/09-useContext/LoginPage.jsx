6c6570240951c2ff988216c52997886c2bdee78b
Miguel Alejandro Salgado Ramírez <alejandrosram@outlook.com>
2022-06-29 13:27:24 -0500

    Update useContext app

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,9 +1,29 @@
+import { useContext } from "react";
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+	const { user, setUser } = useContext(UserContext);
+
 	return (
 		<>
 			<h1>Login Page</h1>
 
 			<hr />
+
+			<pre>{JSON.stringify(user, null, 3)}</pre>
+
+			<button
+				className="btn btn-outline-success"
+				onClick={() =>
+					setUser({
+						id: 1,
+						name: "Alex Salgado",
+						email: "alex@mail.com",
+					})
+				}
+			>
+				Set user
+			</button>
 		</>
 	);
 };
--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,12 +1,13 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AboutPage } from "./AboutPage";
+import { UserProvider } from "./context/UserProvider";
 import { HomePage } from "./HomePage";
 import { LoginPage } from "./LoginPage";
 import { NavBar } from "./NavBar";
 
 export const MainApp = () => {
 	return (
-		<>
+		<UserProvider>
 			<NavBar />
 
 			<hr />
@@ -17,6 +18,6 @@ export const MainApp = () => {
 				<Route path="/login" element={<LoginPage />} />
 				<Route path="/*" element={<Navigate to="/login" />} />
 			</Routes>
-		</>
+		</UserProvider>
 	);
 };
